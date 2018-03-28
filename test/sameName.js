f44d4665b895b1dd916afe08707c5c165ab25897
Admin <admin@Admins-Mac.local>
2018-03-29 00:06:00 +0300

    initial

---
+++ test/sameName.js
@@ -0,0 +1,17 @@
+// const assert = require('assert');
+// describe('same name test', () => {
+//     it('test1', () => {
+//         assert.fail('Fail!')
+//     });
+
+// });
+
+
+
+
+
+// describe('same name 2', () => {
+//     it('test2', () => {
+//         assert.ok("OK")
+//     });
+// });
